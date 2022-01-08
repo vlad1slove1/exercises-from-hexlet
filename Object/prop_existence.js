426eef9554bd6b7b0572babec45a0a31ac827e85
Vladislav Kishchenko <vlad5114@gmail.com>
2022-01-08 19:04:44 +0300

    add second and thrid solutions

--- Object/prop_existence.js
+++ Object/prop_existence.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-prototype-builtins */
 /* eslint-disable no-restricted-syntax */
 
-// This is example of checking for the existence of a property in object. Made by Hexlet teachers
+import has from 'lodash/has.js';
+
+// There are examples of checking for the existence of a property in object. Made by Hexlet teachers
 
 // Create random array. This array contains several fruits
 const bag = [
@@ -9,18 +12,68 @@ const bag = [
   'apple', 'orange', 'pear',
 ];
 
-const fruitsCount = (fruits) => {
+const firstSolution = (fruits) => {
   const result = {};
 
   for (const fruit of fruits) {
-    // check propert for existence
+    // check property for existence
     if (result[fruit] === undefined) {
       result[fruit] = 1;
     } else {
       result[fruit] += 1;
     }
+    // console.log(result);
   }
   return result;
 };
 
-console.log(fruitsCount(bag));
+console.log(firstSolution(bag));
+
+/*
+While traversing the array, this function takes the result object,
+extracts the desired property from it, and increments the value by one.
+*/
+
+const secondSolution = (fruits) => {
+  const result = {};
+
+  for (const fruit of fruits) {
+    // check property for existence
+    if (result.hasOwnProperty(fruit)) {
+      result[fruit] += 1;
+    } else {
+      result[fruit] = 1;
+    }
+  }
+
+  return result;
+};
+
+console.log(secondSolution(bag));
+
+/*
+The hasOwnProperty() method returns a boolean indicating whether the object
+has the specified property as its own property (as opposed to inheriting it).
+*/
+
+const thirdSolution = (fruits) => {
+  const result = {};
+
+  for (const fruit of fruits) {
+    // check property for existence
+    if (has(result, fruit)) {
+      result[fruit] += 1;
+    } else {
+      result[fruit] = 1;
+    }
+  }
+
+  return result;
+};
+
+console.log(thirdSolution(bag));
+
+/*
+Therefore, it is better to check for the existence of the key using the lodash library.
+It contains the has.js function, which haven't the disadvantages of built-in methods.
+*/
