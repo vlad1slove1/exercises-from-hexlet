555f2fb8451901bd1012544ffb491196f9005722
Vladislav Kishchenko <vlad5114@gmail.com>
2022-02-02 21:33:50 +0300

    add map_method.test.js

---
+++ tests/map_method.test.js
@@ -0,0 +1,37 @@
+import solution from '../Functions/map_method.js';
+
+const users = [
+  {
+    name: 'Tirion',
+    children: [
+      { name: 'Mira', birthday: '1983-03-23' },
+    ],
+  },
+  { name: 'Bronn', children: [] },
+  {
+    name: 'Sam',
+    children: [
+      { name: 'Aria', birthday: '2012-11-03' },
+      { name: 'Keit', birthday: '1933-05-14' },
+    ],
+  },
+  {
+    name: 'Rob',
+    children: [
+      { name: 'Tisha', birthday: '2012-11-03' },
+    ],
+  },
+];
+
+const result = [
+  { name: 'Mira', birthday: '1983-03-23' },
+  { name: 'Aria', birthday: '2012-11-03' },
+  { name: 'Keit', birthday: '1933-05-14' },
+  { name: 'Tisha', birthday: '2012-11-03' },
+];
+
+if (solution(users).toString() !== result.toString()) {
+  throw new Error('Function is not working properly!');
+}
+
+console.log('All tests passed!');
