31dc0cc54632adc0413ba2947d8abceb097d50fc
Vladislav Kishchenko <vlad5114@gmail.com>
2022-02-08 23:52:55 +0300

    add secondSolution tests

--- __tests__/array_fill.test.js
+++ __tests__/array_fill.test.js
@@ -1,4 +1,4 @@
-import { firstSolution } from '../Arrays/array_fill.js';
+import { firstSolution, secondSolution } from '../Arrays/array_fill.js';
 
 let array;
 
@@ -8,16 +8,20 @@ beforeEach(() => {
 
 test('value and and start / end items', () => {
   expect(firstSolution(array, '*', 1, 3)).toEqual([1, '*', '*', 4]);
+  expect(secondSolution(array, '*', 1, 3)).toEqual([1, '*', '*', 4]);
 });
 
 test('value only', () => {
   expect(firstSolution(array, '*')).toEqual(['*', '*', '*', '*']);
+  expect(secondSolution(array, '*')).toEqual(['*', '*', '*', '*']);
 });
 
 test('value and start item', () => {
   expect(firstSolution(array, '*', 4)).toEqual([1, 2, 3, 4]);
+  expect(secondSolution(array, '*', 4)).toEqual([1, 2, 3, 4]);
 });
 
 test('value and ending item bigger that array length', () => {
   expect(firstSolution(array, '*', 0, 10)).toEqual(['*', '*', '*', '*']);
+  expect(secondSolution(array, '*', 0, 10)).toEqual(['*', '*', '*', '*']);
 });
