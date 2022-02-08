0607d238f9bb1e77a7f761396774a07ddf3d7055
Vladislav Kishchenko <vlad5114@gmail.com>
2022-02-08 23:48:37 +0300

    fix tests

--- __tests__/array_fill.test.js
+++ __tests__/array_fill.test.js
@@ -1,7 +1,5 @@
 import { firstSolution } from '../Arrays/array_fill.js';
 
-const fill = firstSolution();
-
 let array;
 
 beforeEach(() => {
@@ -9,17 +7,17 @@ beforeEach(() => {
 });
 
 test('value and and start / end items', () => {
-  expect(fill(array, '*', 1, 3)).toEqual([1, '*', '*', 4]);
+  expect(firstSolution(array, '*', 1, 3)).toEqual([1, '*', '*', 4]);
 });
 
 test('value only', () => {
-  expect(fill(array, '*')).toEqual(['*', '*', '*', '*']);
+  expect(firstSolution(array, '*')).toEqual(['*', '*', '*', '*']);
 });
 
 test('value and start item', () => {
-  expect(fill(array, '*', 4)).toEqual([1, 2, 3, 4]);
+  expect(firstSolution(array, '*', 4)).toEqual([1, 2, 3, 4]);
 });
 
 test('value and ending item bigger that array length', () => {
-  expect(fill(array, '*', 0, 10)).toEqual(['*', '*', '*', '*']);
+  expect(firstSolution(array, '*', 0, 10)).toEqual(['*', '*', '*', '*']);
 });
