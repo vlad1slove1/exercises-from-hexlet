8a00efed62c1c1587e5bf5d9af2d72de6cffe670
Vladislav Kishchenko <vlad5114@gmail.com>
2022-02-13 15:52:43 +0300

    add getQuadrant func

--- Functions/Abstractions/cartesian_points.js
+++ Functions/Abstractions/cartesian_points.js
@@ -14,4 +14,29 @@ const getX = (point) => point.x;
 // get Y coordinate
 const getY = (point) => point.y;
 
-export { makeDecartPoint, getX, getY };
+const getQuadrant = (point) => {
+  const x = getX(point);
+  const y = getY(point);
+
+  if (x > 0 && y > 0) {
+    return 1;
+  }
+  if (x < 0 && y > 0) {
+    return 2;
+  }
+  if (x < 0 && y < 0) {
+    return 3;
+  }
+  if (x > 0 && y < 0) {
+    return 4;
+  }
+
+  return null;
+};
+
+export {
+  makeDecartPoint,
+  getX,
+  getY,
+  getQuadrant,
+};
